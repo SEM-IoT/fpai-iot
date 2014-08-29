ad852566b107f1e456dd1be3d0fa761d5244e0bf
Marc de Jonge <marc.dejonge@tno.nl>
2014-08-29 20:35:55 +0200

    Fixed compile errors

--- iotlab.service.warmte.trui/src/org/tno/iotlab/service/warme/trui/DataListner.java
+++ iotlab.service.warmte.trui/src/org/tno/iotlab/service/warme/trui/DataListner.java
@@ -1,9 +1,8 @@
 package org.tno.iotlab.service.warme.trui;
 
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
-import org.eclipse.paho.client.mqttv3.MqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.eclipse.paho.client.mqttv3.MqttTopic;
 
 public class DataListner implements MqttCallback {
 
@@ -14,15 +13,14 @@ public class DataListner implements MqttCallback {
     }
 
     @Override
-    public void deliveryComplete(MqttDeliveryToken arg0) {
+    public void deliveryComplete(IMqttDeliveryToken arg0) {
         // TODO Auto-generated method stub
         System.err.println("delivery complete");
     }
 
     @Override
-    public void messageArrived(MqttTopic arg0, MqttMessage arg1) throws Exception {
+    public void messageArrived(String topic, MqttMessage message) throws Exception {
         // TODO Auto-generated method stub
         System.err.println("gevonden");
     }
-
 }
